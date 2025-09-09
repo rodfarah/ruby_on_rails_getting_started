620c5bc50aeda8f44843fca641b7b87232b9d398
Rodrigo Farah <rodrigo.farah@ferreri.co>
2025-09-09 14:45:29 +0000

    CRUD completed

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,16 +1,26 @@
 class ProductsController < ApplicationController
+  before_action :set_product, only: %i[ show edit update destroy ]
+
   def index
     @products = Product.all
   end
 
   def show
-    @product = Product.find(params[:id])
   end
 
+  # o método new é como "me mostre o formulário vazio"
+  # Por padrão, se você não escrever render ou redirect_to, o Rails vai renderizar automaticamente
+  # a view correspondente ao nome do método: new.html.erb.
   def new
     @product = Product.new
   end
 
+
+  # O método create é executando quando enviamos um formulário pelo html.
+  # Ele cria um objeto product pelos parâmetros enviados pelo formulário,
+  # se os dados forem válidos, há um redirect para o método show,
+  # caso contrário, renderiza novamente o formulário pelo método new,
+  # expondo os erros.
   def create
     @product = Product.new(product_params)
     if @product.save
@@ -20,7 +30,29 @@ class ProductsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @product.update(product_params)
+      redirect_to @product
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @product.destroy
+    redirect_to products_path
+  end
+
+
   private
+  # criar o método abaixo para o before_action
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
   def product_params
     params.expect(product: [ :name ])
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,22 @@
 Rails.application.routes.draw do
+  # resources :products # criaria automaticamente todas as rotas padrão para CRUD
+  # não utilizarei para aprender a fazer as rotas na mão.
   root "products#index" # localhost:3000 levará ao index.
-  get "products/index"
-  resources :products
+
+
+  # new (mostra o formulário em branco) and create (post para o form)
+  get "products/new", to: "products#new", as: "new_product"
+  post "products", to: "products#create"
+
+  # edit and update
+  get "products/:id/edit", to: "products#edit", as: "edit_project"
+  patch "products/:id", to: "products#update"
+  put "products/:id", to: "products#update"
+
+  # Index (list) and show (only one)
+  get "products", to: "products#index"
+  get "products/:id", to: "products#show", as: "product"
+
+  # Delete items
+  delete "products/:id", to: "products#destroy", as: "destroy_product"
 end
